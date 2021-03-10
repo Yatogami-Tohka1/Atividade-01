ba89de247a0bd771d29cd155024d7984ef12a392
Yatogami-Tohka1 <79715828+Yatogami-Tohka1@users.noreply.github.com>
2021-03-10 14:20:33 -0300

    Adicionado fim

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Fim!");
         }
     }
 }
