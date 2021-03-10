62481765cff48818049099bdf1c0aae1b262dd3f
Yatogami-Tohka1 <79715828+Yatogami-Tohka1@users.noreply.github.com>
2021-03-10 14:29:05 -0300

    Removido Espanhol

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
             Console.WriteLine("Fim!");
         }
     }
