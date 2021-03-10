1e990e004dcd45ed79f63a22ea37d191b808842e
Yatogami-Tohka1 <79715828+Yatogami-Tohka1@users.noreply.github.com>
2021-03-10 14:27:34 -0300

    Removido Hello World

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Hola Mundo!");
             Console.WriteLine("Fim!");
